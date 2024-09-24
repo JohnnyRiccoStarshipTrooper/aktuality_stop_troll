9ba664e5753de780b3a96e2acc91c332bb27c671
JohnnyRiccoStarshipTrooper <102032058+JohnnyRiccoStarshipTrooper@users.noreply.github.com>
2024-09-24 06:48:39 +0200

    Update aktuality_stop_troll.js

--- aktuality_stop_troll.js
+++ aktuality_stop_troll.js
@@ -4,7 +4,7 @@
 // ==UserScript==
 // @name         Blokovanie trollov na aktuality.sk
 // @namespace    http://tampermonkey.net/
-// @version      0.32
+// @version      0.33
 // @description  try to take over the world!
 // @author       StarshipTrooper
 // @match        https://www.aktuality.sk/diskusia/*
@@ -32,48 +32,47 @@
 		height:30px;
 		margin-left: 10px;
 	`;
-
-	$.each($("div.d-comment-wrapper"), function() {
-		var message_id = $(this).attr("data-comment-id");
-		var user_id = $(this).attr("data-azet_user_id");
-		//var user_name = $(this).find("span.username").text();
-
-		$(this).parent().attr("data-comm_id",message_id);
-		$(this).parent().attr("data-user_id",user_id);
-		//$(this).parent().attr("data-user_name",user_name);
-
-		// ban/unban button
-		if (localStorage.getItem("ban_user_"+user_id) === null) {
-			$(this).find("div.d-buttons").append(`<span style="`+button_css+`" class="ban" onclick="javascript:localStorage.setItem('ban_user_`+user_id+`',1);location.reload();">BAN</span>`);
-		} else {
-			troll_messages.push(message_id);
-			$(this).find("div.user-img-wrapper").html(`<span style="font-size:30px;">&#129484;</span>`);
-            		$(this).find("div.user-img-wrapper").css({"width":"52px","text-align":"center"});
-			$(this).find("div.d-buttons").append(`<span style="`+button_css+`" onclick="javascript:localStorage.removeItem('ban_user_`+user_id+`');location.reload();">UNBAN</span>`);
-			$(this).find("div.d-buttons").append(`<span style="`+button_css+`" onclick="javascript:$(this).parent().parent().parent().parent().find('div.d-body').toggle();">&#128065;</span>`);
-			$(this).css({"background":"#fcfcfa","margin-bottom":"2px"});
-			$(this).find("div.d-body").css("color","#877");
-			$(this).find("div.d-body").hide();
-		}
-	});
-	$.each(troll_messages, function(index,value) {
-		var element = $("div.d-anchor-wrapper[data-comm_id='"+value+"']");
-		setTimeout(
-			function() {
-				var disliked_messages = JSON.parse(localStorage.getItem("disliked_messages") || "[]");
-				var msg_idx = disliked_messages.indexOf(value);
-				if (msg_idx === -1) {
-					disliked_messages.push(value);
-					if (disliked_messages.length > 500) {
-						disliked_messages.shift();
+	if ($("#account-login-dropdown .account-title-wrapper #account-button-title").text() != "Prihlásiť") {
+		$.each($("div.d-comment-wrapper"), function() {
+			var message_id = $(this).attr("data-comment-id");
+			var user_id = $(this).attr("data-azet_user_id");
+			$(this).parent().attr("data-comm_id",message_id);
+			$(this).parent().attr("data-user_id",user_id);
+			// ban/unban button
+			if (localStorage.getItem("ban_user_"+user_id) === null) {
+				$(this).find("div.d-buttons").append(`<span style="`+button_css+`" class="ban" onclick="javascript:localStorage.setItem('ban_user_`+user_id+`',1);location.reload();">BAN</span>`);
+			} else {
+				troll_messages.push(message_id);
+				$(this).find("div.user-img-wrapper").html(`<span style="font-size:30px;">&#129484;</span>`);
+				$(this).find("div.user-img-wrapper").css({"width":"52px","text-align":"center"});
+				$(this).find("div.d-buttons").append(`<span style="`+button_css+`" onclick="javascript:localStorage.removeItem('ban_user_`+user_id+`');location.reload();">UNBAN</span>`);
+				$(this).find("div.d-buttons").append(`<span style="`+button_css+`" onclick="javascript:$(this).parent().parent().parent().parent().find('div.d-body').toggle();">&#128065;</span>`);
+				$(this).css({"background":"#fcfcfa","margin-bottom":"2px"});
+				$(this).find("div.d-body").css("color","#877");
+				$(this).find("div.d-body").hide();
+			}
+		});
+		$.each(troll_messages, function(index,value) {
+			var element = $("div.d-anchor-wrapper[data-comm_id='"+value+"']");
+			setTimeout(
+				function() {
+					var disliked_messages = JSON.parse(localStorage.getItem("disliked_messages") || "[]");
+					var msg_idx = disliked_messages.indexOf(value);
+					if (msg_idx === -1) {
+						disliked_messages.push(value);
+						if (disliked_messages.length > 500) {
+							disliked_messages.shift();
+						}
+						localStorage.setItem("disliked_messages", JSON.stringify(disliked_messages));
+						element.find("span.d-dislike").click();
+						element.find("span.d-dislike").css("color","#822");
 					}
-					localStorage.setItem("disliked_messages", JSON.stringify(disliked_messages));
-					element.find("span.d-dislike").click();
-					element.find("span.d-dislike").css("color","#822");
-				}
-				element.find("span.d-dislike").css("background","#ff8");
-			},
-			(index*800)
-		);
-	});
+					element.find("span.d-dislike").css("background","#ff8");
+				},
+				(index*800)
+			);
+		});
+	} else {
+        console.log("Neprihlásený");
+    }
 })();
